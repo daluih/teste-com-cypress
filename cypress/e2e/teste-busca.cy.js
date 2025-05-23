669a6291dd91473b50e36651dccfaeda8079a1d0
daluih <dara4lemos@gmail.com>
2025-05-23 17:38:36 -0400

    ajuste teste de busca

--- cypress/e2e/teste-busca.cy.js
+++ cypress/e2e/teste-busca.cy.js
@@ -1,7 +1,7 @@
 describe('Busca no DuckDuckGo', () => {
   it('deve buscar por Cypress.io e encontrar o site oficial', () => {
-    cy.visit('https://duckduckgo.com') // abre o site
-    cy.get('input[name="q"]').type('Cypress.io{enter}') // digita no campo e aperta Enter
-    cy.contains('https://www.cypress.io').should('exist') // verifica se o link do Cypress aparece
-  })
-})
+    cy.visit('https://duckduckgo.com/');
+    cy.get('input[name="q"]').type('Cypress.io{enter}');
+    cy.contains('cypress.io', { timeout: 10000 }).should('be.visible');
+  });
+});
